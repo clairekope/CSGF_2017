302c979b88a8ffa549301f09ac4345fde19221db
miorteg <miorteg@berkeley.edu>
2017-07-26 11:16:19 -0400

    Compiling Mandelbrot and modified Makefile

--- Kokkos/Mandelbrot/Mandelbrot.cpp
+++ Kokkos/Mandelbrot/Mandelbrot.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <stdlib>
+#include <stdlib.h>
 #include <Kokkos_Core.hpp>
 #include <Kokkos_Parallel.hpp>
 #include <Kokkos_View.hpp>
@@ -7,6 +7,7 @@
 //#include <assert.h>
 //#include <limits> 
 using std::cout;
+using std::endl;
 
 int main(int argc, char **argv) {
   // Initialize MPI before Kokkos
@@ -18,14 +19,14 @@ int main(int argc, char **argv) {
   // Initialze grid properties from command line
   if (argc < 5){
     cout << "Need x, y, len_x, len_y" << endl;
-    std::exit();
+    exit(1);
   }
 
   float cen_x, cen_y, len_x, len_y;
-  cen_x = std::stof(std::to_string(argv[1]));
-  cen_y = std::stof(std::to_string(argv[2]));
-  len_x = std::stof(std::to_string(argv[3]));
-  len_y = std::stof(std::to_string(argv[4]));
+  cen_x = std::atoi(argv[1]);
+  cen_y = std::atoi(argv[2]);
+  len_x = std::atoi(argv[3]);
+  len_y = std::atoi(argv[4]);
 
   Kokkos::finalize();
 
